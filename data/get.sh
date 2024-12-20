28f66a0b1de60f6221fd67f169b23a32c97e8097
David J. H. Shih <djh.shih@gmail.com>
2024-12-20 19:09:21 +0800

    Download additional files.

--- data/get.sh
+++ data/get.sh
@@ -5,4 +5,11 @@ set -euo pipefail
 IFS=$'\n\t'
 
 #curl -O https://ftp.expasy.org/databases/cellosaurus/cellosaurus.obo
-curl -O https://ftp.expasy.org/databases/cellosaurus/cellosaurus.txt
+# OBO format file does not contain:
+#   the STR profile data
+#   the age at sampling
+#   the full reference records (authors, title, journal)
+
+#curl -O https://ftp.expasy.org/databases/cellosaurus/cellosaurus.txt
+curl -O https://ftp.expasy.org/databases/cellosaurus/cellosaurus_refs.txt
+curl -O https://ftp.expasy.org/databases/cellosaurus/cellosaurus_xrefs.txt
